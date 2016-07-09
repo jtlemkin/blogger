14add5676371109064a99f7923b91b76e69e2361
jtlemkin <jtlemkin@gmail.com>
2016-07-08 20:17:32 -0700

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,47 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
+
 	def index
 		@articles = Article.all
 	end
 
 	def show
 		@article = Article.find(params[:format])	
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+
+		@article.save
+
+		flash.notice = "Article '#{@article.title}' Created!"
+
+		redirect_to @article
+	end
+
+	def destroy
+		@article = Article.find(params[:format])
+		@article.destroy
+
+		flash.notice = "Article '#{@article.title}' Destroyed!"
+
+		redirect_to @article
+	end
+
+	def edit
+		@article = Article.find(params[:format])
+	end
+
+	def update
+		@article = Article.find(params[:format])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' Updated!"
+
+		redirect_to @article
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  #root to: "articles#index"
+  root to: "articles#index"
 
   get '/articles', to: 'articles#index'
+  post '/articles', to: "articles#create"
 
   resource :article
 end
